e4ea1f8ded58d304b9594921a00d6edb29f2d0c7
Dmitry Zharinov <zharinovdm@yandex.ru>
2022-04-06 08:05:31 +0000

    add user interaction

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 import os
 import requests
 from dotenv import load_dotenv
-from pprint import pprint
 from urllib.parse import urlparse
+
+BITLY_API = 'https://api-ssl.bitly.com/v4'
 
 
 def get_token():
     return os.environ['BITLY_TOKEN']
 
 
-def get_bitly_profile(token):
-    api_url = "https://api-ssl.bitly.com/v4/user"
-    headers = {
-        'Authorization': f"Bearer {token}",
-        'Content-Type': 'application/json',
-    }
-    #data = '{ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" }'
-    #response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
-    response = requests.get(api_url, headers=headers)
-    response.raise_for_status()
-    return response.text
-
-
 def shorten_link(token, url):
-    api_url = "https://api-ssl.bitly.com/v4/shorten"
+    api_url = f"{BITLY_API}/shorten"
     headers = {
         'Authorization': f"Bearer {token}",
         'Content-Type': 'application/json',
@@ -38,14 +26,12 @@ def shorten_link(token, url):
 
 
 def count_clicks(token, bitlink):
-    bitlink_parsed = urlparse(bitlink)
-    
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink_parsed.netloc}{bitlink_parsed.path}/clicks/summary'
+    url = urlparse(bitlink)
+    api_url = f'{BITLY_API}/bitlinks/{url.netloc}{url.path}/clicks/summary'
     headers = {
         'Authorization': f"Bearer {token}",
         'Content-Type': 'application/json',
     }
-    
     response = requests.get(api_url, headers=headers)
     response.raise_for_status()
     count_clicks = response.json()['total_clicks']
@@ -54,7 +40,7 @@ def count_clicks(token, bitlink):
 
 def is_bitlink(token, url):
     url_parsed = urlparse(url)
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url_parsed.netloc}{url_parsed.path}'
+    api_url = f'{BITLY_API}/bitlinks/{url_parsed.netloc}{url_parsed.path}'
     headers = {
         'Authorization': f"Bearer {token}",
         'Content-Type': 'application/json',
@@ -65,20 +51,16 @@ def is_bitlink(token, url):
 
 def main():
     load_dotenv()
-    #url = 'https://dvmn.org'
-    #url = input('Введите url для сокращения: ')
-
     token = get_token()
-    try:
-        #bitlink = shorten_link(token, url)
-        #print('Битлинк', bitlink)
-        clicks_count = count_clicks(token, 'https://bit.ly/3JcgkHq')
-        print(clicks_count)
-    except requests.exceptions.HTTPError as err:
-        print(f'Ошибка при создании битлинка:\n{err}')
+    url = input('Введите ссылку: ')
+    if is_bitlink(token, url):
+        print(f'По вашей ссылке прошли: {count_clicks(token, url)} раз(а)')
+    else:
+        try:
+            print('Битлинк: ', shorten_link(token, url))
+        except requests.exceptions.HTTPError as err:
+            print(f'Ошибка при создании битлинка:\n{err}')
 
 
 if __name__ == '__main__':
     main()
-    
-    
