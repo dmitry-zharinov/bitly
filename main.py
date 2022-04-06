baabda9a55492f13151269a6c1e817a6fb1c71d1
Dmitry Zharinov <zharinovdm@yandex.ru>
2022-04-06 18:04:50 +0000

    add check link function & code improvements

--- main.py
+++ main.py
@@ -1,63 +1,59 @@
 import os
+from urllib.parse import urlparse
+
 import requests
 from dotenv import load_dotenv
-from urllib.parse import urlparse
 
 BITLY_API = 'https://api-ssl.bitly.com/v4'
 
 
-def get_token():
-    return os.environ['BITLY_TOKEN']
+def check_link(url):
+    response = requests.get(url)
+    response.raise_for_status()
 
 
-def shorten_link(token, url):
+def shorten_link(headers, url):
     api_url = f"{BITLY_API}/shorten"
-    headers = {
-        'Authorization': f"Bearer {token}",
-        'Content-Type': 'application/json',
-    }
     data = {
         'long_url': url,
     }
     response = requests.post(api_url, headers=headers, json=data)
     response.raise_for_status()
-    bitlink = response.json()['link']
-    return bitlink
+    return response.json()['link']
 
 
-def count_clicks(token, bitlink):
+def count_clicks(headers, bitlink):
     url = urlparse(bitlink)
     api_url = f'{BITLY_API}/bitlinks/{url.netloc}{url.path}/clicks/summary'
-    headers = {
-        'Authorization': f"Bearer {token}",
-        'Content-Type': 'application/json',
-    }
     response = requests.get(api_url, headers=headers)
     response.raise_for_status()
-    count_clicks = response.json()['total_clicks']
-    return count_clicks
+    return response.json()['total_clicks']
 
 
-def is_bitlink(token, url):
+def is_bitlink(headers, url):
     url_parsed = urlparse(url)
     api_url = f'{BITLY_API}/bitlinks/{url_parsed.netloc}{url_parsed.path}'
-    headers = {
-        'Authorization': f"Bearer {token}",
-        'Content-Type': 'application/json',
-    }
     response = requests.get(api_url, headers=headers)
     return response.ok
 
 
 def main():
     load_dotenv()
-    token = get_token()
+    headers = {
+        'Authorization': f"Bearer {os.environ['BITLY_TOKEN']}",
+    }
     url = input('Введите ссылку: ')
-    if is_bitlink(token, url):
-        print(f'По вашей ссылке прошли: {count_clicks(token, url)} раз(а)')
+    try:
+        check_link(url)
+    except:
+        print('Введена некорректная ссылка')
+        exit()
+
+    if is_bitlink(headers, url):
+        print(f'По вашей ссылке прошли: {count_clicks(headers, url)} раз(а)')
     else:
         try:
-            print('Битлинк: ', shorten_link(token, url))
+            print('Битлинк: ', shorten_link(headers, url))
         except requests.exceptions.HTTPError as err:
             print(f'Ошибка при создании битлинка:\n{err}')
 
